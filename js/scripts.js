d44c0ab8729a302c194ae2aa3f3435e0c45a8090
praxyfarhana <praxyfarhana@gmail.com>
2018-07-05 16:06:50 +0300

    styled

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
-  $("p").click(function() {
+  $("h4").click(function() {
     $("p").show();
   });
 });
+
+$(document).ready(function() {
+      $(".clickable").click(function() {
+        $("h4").toggle();
+        $("h4").toggle();
+      });
+    });
